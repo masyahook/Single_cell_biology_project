d2ad8da57123646ce7672d19cd08d3883d8f4d7c
maximkryukov <kryukov.ma@phystech.edu>
2020-03-28 02:58:27 +0100

    added wilcoxon, better description

--- Project_analysis.R
+++ Project_analysis.R
@@ -4,9 +4,11 @@ library(dplyr)
 library(ggplot2)
 
 # Reading the file
-raw_counts <- read.table(file='/Users/MaximKryukov/Documents/GitHub/Single_cell_biology/MOUSE_BRAIN_DATASET_2_COUNTS.tsv')
+raw_counts <- read.table(file='/Users/MaximKryukov/Documents/GitHub/Single_cell_biology_project/data/MOUSE_BRAIN_DATASET_2_COUNTS.tsv')
 mouse_rawdata <- CreateSeuratObject(counts = raw_counts, project = 'Mouse_cell_atlas')
 sprintf('The total number of genes: %d. The total number of cells/samples: %d', dim(mouse_rawdata)[1], dim(mouse_rawdata)[2])
+
+### DATA MANAGEMENT
 
 # Plotting number of UMI counts for each cell
 min_UMI_count = 600
@@ -26,6 +28,8 @@ sprintf('The median number of detected genes per cell: %f. The mean number of de
         median(mouse_rawdata_filtered$nFeature_RNA), mean(mouse_rawdata_filtered$nFeature_RNA))
 sprintf('The median number of UMI counts per cell: %f. The mean number of UMI counts per cell: %f', 
         median(mouse_rawdata_filtered$nCount_RNA), mean(mouse_rawdata_filtered$nCount_RNA))
+
+### FILTERING
 
 ### Now let's plot some statistic about our dataset
 
@@ -48,8 +52,7 @@ nFeature_count_max = 4000
 mouse_data <- subset(mouse_rawdata_filtered, subset = (nFeature_RNA > nFeature_count_min) & (nFeature_RNA < nFeature_count_max) & (percent.mt < mt_percent_max))
 sprintf('The total number of genes: %d. The total number of cells/samples: %d', dim(mouse_data)[1], dim(mouse_data)[2])
 
-### We use default Seurat normalization, 
-### because in the downstream analysis we see that everything is distinct
+### NORMALIZATION
 
 # We normalize data with by correcting the sequencing depth and log transformatting
 mouse_data <- NormalizeData(mouse_data, normalization.method = "LogNormalize", scale.factor = 10000)
@@ -61,13 +64,14 @@ mouse_data <- FindVariableFeatures(mouse_data, selection.method = "vst", nfeatur
 top10_variable_genes <- head(VariableFeatures(mouse_data), 10)
 
 # Plot variable features with and without labels
-plot1 <- VariableFeaturePlot(mouse_data)
-plot2 <- LabelPoints(plot = plot1, points = top10_variable_genes, repel = TRUE)
-CombinePlots(plots = list(plot1, plot2))
+variable_feature_plot <- VariableFeaturePlot(mouse_data)
+LabelPoints(plot = variable_feature_plot, points = top10_variable_genes, repel = TRUE)
 
 # Do the standardization (z-scoring) of the data
-all.genes <- rownames(mouse_data)
-mouse_data <- ScaleData(mouse_data, features = all.genes)
+all_genes <- rownames(mouse_data)
+mouse_data <- ScaleData(mouse_data, features = all_genes)
+
+# DIMENSIONALITY REDUCTION
 
 # Now we can find the principal components to find subtypes of cells
 mouse_data <- RunPCA(mouse_data, features = VariableFeatures(object = mouse_data))
@@ -89,6 +93,8 @@ mouse_data <- JackStraw(mouse_data, num.replicate = 100)
 mouse_data <- ScoreJackStraw(mouse_data, dims = 1:20)
 JackStrawPlot(mouse_data, dims = 1:20)
 
+### CLUSTERING
+
 # Now we can cluster cells using Phenograph 
 mouse_data <- FindNeighbors(mouse_data, dims = 1:12)
 mouse_data <- FindClusters(mouse_data, resolution = 0.5)
@@ -97,25 +103,38 @@ mouse_data <- FindClusters(mouse_data, resolution = 0.5)
 mouse_data <- RunUMAP(mouse_data, dims = 1:12)
 DimPlot(mouse_data, reduction = "umap")
 
+### TECHNICAL AND BIOLOGICAL BIAS DETECTION
+
 # Detection for 3 different technical biases:
 # 1) the sequencing depth (number of counts for each cell)
 # 2) the cell cycle bias
 # 3) the drouplet effect
 # 4) the mitochondrial genes (dying cells)
-FeaturePlot(mouse_data, features = c("percent.mt", 'nFeature_RNA', 'nCount_RNA'))
 
-FeaturePlot(mouse_data, features = c('Neurod1', 'Gm2694', 'Cbln3', 'Gabra6', 'Snap25', 'Cbln1', 'Adcy1', 
-                                     'Nrxn1', 'Scn1b', 'Kcnc1', 'Nrxn3', 'Camk4', 'Rbfox3', 'Pkib', 'Chd7'))
+# Plotting UMAP with 3 feautre coloring
+FeaturePlot(mouse_data, features = c('nFeature_RNA', 'nCount_RNA', "percent.mt"), ncol=3)
 
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.gene
-mouse_data <- CellCycleScoring(mouse_data, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+# Getting cell-cycle canonical gene markers
+s_genes <- cc.genes$s.genes
+g2m_genes <- cc.genes$g2m.gene
 
-mouse_data <- RunPCA(mouse_data, features = c(s.genes, g2m.genes))
-DimPlot(mouse_data)
+# Getting a subset that is present in mouse_data from initial list of gene markers
+s_genes_present <- intersect(s_genes, all_genes)
+g2m_genes_present <- intersect(g2m_genes, all_genes)
+
+# The arrays above are empty -> no cell-cycle effect!
+
+### DIFFERENTIAL EXPRESSION ANALYSIS
 
 # Find markers for every cluster compared to all remaining cells, report only the positive ones
 mouse_data.markers <- FindAllMarkers(mouse_data, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, test.use='t')
-de_genes <- data.frame(mouse_data.markers %>% group_by(cluster) %>% top_n(n = 15, wt = avg_logFC))
+de_genes <- data.frame(mouse_data.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_logFC))
 
-FeaturePlot(mouse_data, features = c('Npy', 'Prph'))
+# Heatmap expression plot for variable genes for each cluster
+top10 <- mouse_data.markers  %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+DoHeatmap(mouse_data, features = top10$gene) + NoLegend()
+
+# Let us try to do the same but with Wilcoxon test
+mouse_data.markers_wilcoxon <- FindAllMarkers(mouse_data, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+de_genes_wilcoxon <- data.frame(mouse_data.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_logFC))
+
